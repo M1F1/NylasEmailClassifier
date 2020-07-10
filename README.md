e2673fcc96acaacbbbc859fe4eb2dbdfd9096da9
M1F1 <filek.michal@gmail.com>
2020-07-10 10:40:01 +0200

    add documentation, main function

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# NylasEmailClassifier
+## NylasEmailClassifier
+1. Clone repo and install requiremets: 
+  ```sh
+  git clone 
+  cd /NylasEmailClassifier
+  pip install -r requirements.txt
+  ```
+
+2. Copy all required credentials from your [Nylas account](https://www.nylas.com/) to `.env` file (disclaimer - only gmail is supported).
+3. Decide what is the new category/label which you want add to some of received messages.
+4. Create training dataset from current messages in your mail account (you can specify number of messages, default is 100):
+  ```sh
+  python create_train_predict.py -l {new_label_name} -nt 50 -c
+  ```
+5. In the next step you will be training binary classifier, so tag messages either with 0 or 1 (1 means that message should be tagged with chosen new label).
+   Save tagged data as `{new_label_name}_train_labeled_dataset.csv`.
+
+6. Train classifier on tagged data:
+  ```sh
+  python create_train_predict.py -l {new_label_name} -t
+  ```
+7. Classify the newest received email message:
+  ```sh
+  python create_train_predict.py -l {new_label_name} -p NylasEmailClassifier
--- create_train_predict.py
+++ create_train_predict.py
@@ -14,110 +14,117 @@ import joblib
 # TODO add documentation
 # TODO add test for preprocessing function
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--new_label_name",
-                    help="label name for a new folder in your mail")
-parser.add_argument("-nt", "--number_of_training_samples", type=int,
-                    default=100, help="decide how many email messages "
-                    "will be use for training your classification model")
-parser.add_argument("-c", "--create_train_dataset", action='store_true',
-                    help="create csv file with processed email messages")
-parser.add_argument("-t", "--train_model", action="store_true",
-                    help="train binary classifier which will classify new label again rest")
-parser.add_argument("-p", "--predict_label", action="store_true",
-                    help="predict the newest received email label")
-args = parser.parse_args()
-
-load_dotenv()
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-
-nylas = APIClient(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    ACCESS_TOKEN
-)
-
 def preprocessing(body_html):
     text = BeautifulSoup(body_html, "lxml").text
     return " ".join(text.split())
 
-if args.create_train_dataset:
-    print('Creating training dataset ... ', end='')
-    messages = nylas.messages.all(limit=args.number_of_training_samples)
-    messages_bodies = [message.body for message in messages]
-    tags = [None] * len(messages_bodies)
-    df = pd.DataFrame({'text': messages_bodies,
-                       'tag': tags})
+def main(args):
 
-    df['text'] = df['text'].apply(preprocessing)
-    df.to_csv(f'{args.new_label_name}_train_dataset.csv')
-    print('done.')
-    print(df.info())
+    load_dotenv()
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 
-    print(f'You have to annotate messages in {args.new_label_name}_train_dataset.csv '
-           'either with 0 or 1 tag, where 1 means that message '
-          f'should be moved to {args.new_label_name} folder in your email account.')
+    nylas = APIClient(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        ACCESS_TOKEN
+    )
 
-if args.train_model:
-    try:
-        df = pd.read_csv(f'{args.new_label_name}_train_labeled_dataset.csv')
-    except:
-        print(f"You have to create labels for every message in {args.new_label_name}_train_dataset.csv"
-              f"and saved it as {args.new_label_name}_train_labeled_dataset.csv")
-        raise
 
-    print('Number of samples for each class: ')
-    print(df.groupby(['tag']).count())
-    df.dropna(subset=['tag'], inplace=True)
-    y = df['tag']
-    x = df['text']
+    if args.create_train_dataset:
+        print('Creating training dataset ... ', end='')
+        messages = nylas.messages.all(limit=args.number_of_training_samples)
+        messages_bodies = [message.body for message in messages]
+        tags = [None] * len(messages_bodies)
+        df = pd.DataFrame({'text': messages_bodies,
+                        'tag': tags})
 
-    text_clf = Pipeline([
-      ('vect', CountVectorizer()),
-      ('tfidf', TfidfTransformer()),
-      ('clf', MultinomialNB()),
-    ])
+        df['text'] = df['text'].apply(preprocessing)
+        df.to_csv(f'{args.new_label_name}_train_dataset.csv')
+        print('done.')
+        print(df.info())
 
-    print('Training Model ... ', end='')
-    text_clf.fit(x, y)
-    print('done.')
+        print(f'You have to annotate messages in {args.new_label_name}_train_dataset.csv '
+               'either with 0 or 1 tag, where 1 means that message '
+              f'should be moved to {args.new_label_name} folder in your email account.')
 
-    joblib.dump(text_clf, f'{args.new_label_name}_model.pkl', compress = 1)
+    if args.train_model:
+        try:
+            df = pd.read_csv(f'{args.new_label_name}_train_labeled_dataset.csv')
+        except:
+            print(f"You have to create labels for every message in {args.new_label_name}_train_dataset.csv"
+                f"and saved it as {args.new_label_name}_train_labeled_dataset.csv")
+            raise
 
-if args.predict_label:
-    try:
-        text_clf = joblib.load(f'{args.new_label_name}_model.pkl')
+        print('Number of samples for each class: ')
+        print(df.groupby(['tag']).count())
+        df.dropna(subset=['tag'], inplace=True)
+        y = df['tag']
+        x = df['text']
 
-    except:
-        print('First create train dataset, tag it and train model '
-              'before making any prediction.')
-        raise
+        text_clf = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB()),
+        ])
 
-    if nylas.account.organization_unit == 'label':
+        print('Training Model ... ', end='')
+        text_clf.fit(x, y)
+        print('done.')
 
-        labels = nylas.labels.all()
+        joblib.dump(text_clf, f'{args.new_label_name}_model.pkl', compress = 1)
 
-        label_list = list(filter(lambda x: x.display_name == args.new_label_name, labels))
+    if args.predict_label:
+        try:
+            text_clf = joblib.load(f'{args.new_label_name}_model.pkl')
 
-        if not label_list:
-            my_new_label = nylas.labels.create()
-            my_new_label.display_name = args.new_label_name
-            my_new_label.save()
+        except:
+            print('First create train dataset, tag it and train model '
+                  'before making any prediction.')
+            raise
+
+        if nylas.account.organization_unit == 'label':
+
+            labels = nylas.labels.all()
+
+            label_list = list(filter(lambda x: x.display_name == args.new_label_name, labels))
+
+            if not label_list:
+                my_new_label = nylas.labels.create()
+                my_new_label.display_name = args.new_label_name
+                my_new_label.save()
+            else:
+                my_new_label = label_list[0]
+
+            message = nylas.messages.first()
+            text = preprocessing(message.body)
+
+            print('Predicting label for: ')
+            print(text)
+            predicted_label = text_clf.predict([text])
+            print('Predicted label: ', predicted_label[0])
+
+            if predicted_label[0] == 1.:
+                print(f'Message now is in {args.new_label_name} folder.')
+                message.add_label(my_new_label.id)
         else:
-            my_new_label = label_list[0]
+            print('Supporting Gmail API only.')
 
-        message = nylas.messages.first()
-        text = preprocessing(message.body)
+if __name__ == '__main__':
 
-        print('Predicting label for: ')
-        print(text)
-        predicted_label = text_clf.predict([text])
-        print('Predicted label: ', predicted_label[0])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--new_label_name",
+                        help="label name for a new folder in your mail")
+    parser.add_argument("-nt", "--number_of_training_samples", type=int,
+                        default=100, help="decide how many email messages "
+                        "will be use for training your classification model")
+    parser.add_argument("-c", "--create_train_dataset", action='store_true',
+                        help="create csv file with processed email messages")
+    parser.add_argument("-t", "--train_model", action="store_true",
+                        help="train binary classifier which will classify new label again rest")
+    parser.add_argument("-p", "--predict_label", action="store_true",
+                        help="predict the newest received email label")
+    args = parser.parse_args()
 
-        if predicted_label[0] == 1.:
-            print(f'Message now is in {args.new_label_name} folder.')
-            message.add_label(my_new_label.id)
-    else:
-        print('Supporting Gmail API only.')
+    main(args)
